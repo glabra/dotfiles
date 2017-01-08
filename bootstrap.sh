1acfd15fbe438f9858585e2b765b460f605ce483
INOUE Yosuke <inoue@kamikakushi.net>
2017-01-08 14:24:40 +0900

    commonな部分を纏められるようにした

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,141 +10,163 @@ SRCTYPE_FILE="${ROOT}/.src_type"
 DESTINATION="${HOME}"
 
 die () {
-    cat << _EOF_
-usage: $0 COMMAND
+	cat <<- _EOF_
+	usage: $0 COMMAND
 
-The following commands are available:
-  install [src_type]:
-    install dotfiles into ~/
-  uninstall:
-    uninstall dotfiles from ~/
-  sync:
-    remove unresolvable symlinks
-  help:
-    show this text.
+	The following commands are available:
+	  install [src_type]:
+	    install dotfiles into ~/
+	  uninstall:
+	    uninstall dotfiles from ~/
+	  sync:
+	    remove unresolvable symlinks
+	  help:
+	    show this text.
 
-Default command is [install].
-_EOF_
-    exit $1
+	Default command is [install].
+	_EOF_
+	exit $1
 }
 
 create_symlinks () {
-    local srcdir=$1
-    local destdir=$2
+	(
+	srcdir=$1
+	destdir=$2
 
-    find "${srcdir}" -type d | \
-        sed "s%^${srcdir}/%${destdir}/.%" | \
-        xargs mkdir -p >/dev/null 2>&1
+	find "${srcdir}" -type d | \
+		sed "s%^${srcdir}/%${destdir}/.%" | \
+		xargs mkdir -p >/dev/null 2>&1
 
-    local files=$(find "${srcdir}" -type f)
-    for file in ${files}; do
-        dest="${destdir}/.${file#${srcdir}/}"
-        [ ! -L "${dest}" ] && ln -s -- "${file}" "${dest}"
-    done
-    unset dest
-    unset file
+	files=$(find "${srcdir}" -type f)
+	for file in ${files}; do
+		dest="${destdir}/.${file#${srcdir}/}"
+		[ ! -L "${dest}" ] && ln -s -- "${file}" "${dest}"
+	done
+
+	return 0
+	)
 }
 
 remove_symlinks () {
-    local srcdir=$1
-    local destdir=$2
+	(
+	srcdir=$1
+	destdir=$2
 
-    local files=$(find "${srcdir}" -type f)
-    for file in ${files}; do
-        dest="${destdir}/.${file#${srcdir}/}"
-        [ -L "${dest}" ] && rm -f "${dest}"
-    done
-    unset dest
-    unset file
+	files=$(find "${srcdir}" -type f)
+	for file in ${files}; do
+		dest="${destdir}/.${file#${srcdir}/}"
+		[ -L "${dest}" ] && rm -f "${dest}"
+	done
 
-    find "${srcdir}" -type d | \
-        sed "s%^${srcdir}/%${destdir}/.%" | \
-        xargs rmdir -p >/dev/null 2>&1
+	find "${srcdir}" -type d | \
+		sed "s%^${srcdir}/%${destdir}/.%" | \
+		xargs rmdir -p >/dev/null 2>&1
+
+	return 0
+	)
 }
 
 remove_dangling_symlinks () {
-    local srcdir=$1
-    local destdir=$2
+	(
+	srcdir=$1
+	destdir=$2
 
-    local files=$(find "${destdir}" -type l)
-    for file in ${files}; do
-        realfile=$(readlink "${file}") && \
-            printf '%s' "${realfile}" | fgrep -q "${srcdir}" && \
-            [ ! -e "${realfile}" ] && \
-            rm -f -- "${file}"
-    done
-    unset realfile
-    unset file
+	files=$(find "${destdir}" -type l)
+	for file in ${files}; do
+		realfile=$(readlink "${file}") && \
+			printf '%s' "${realfile}" | fgrep -q "${srcdir}" && \
+			[ ! -e "${realfile}" ] && \
+			rm -f -- "${file}"
+	done
 
-    find "${srcdir}" -type d | \
-        sed "s%^${srcdir}/%${destdir}/.%" | \
-        xargs rmdir -p >/dev/null 2>&1
+	find "${srcdir}" -type d | \
+		sed "s%^${srcdir}/%${destdir}/.%" | \
+		xargs rmdir -p >/dev/null 2>&1
+
+	return 0
+	)
 }
 
 get_srctype () {
-    [ -n "${1:-}" ] && \
-        printf -- '%s' "${1}" > "${SRCTYPE_FILE}"
+	(
+	[ -n "${1:-}" ] && \
+		printf -- '%s' "${1}" > "${SRCTYPE_FILE}"
 
-    [ -e "${SRCTYPE_FILE}" ] && \
-        src_type=$(cat "${SRCTYPE_FILE}")
+	[ -e "${SRCTYPE_FILE}" ] && \
+		src_type=$(cat "${SRCTYPE_FILE}")
 
-    [ -z "${src_type:-}" ] && \
-        printf -- 'No src_type defined.\n' && \
-        exit 1
+	[ -z "${src_type:-}" ] && \
+		printf -- 'No src_type defined.\n' && \
+		exit 1
 
-    printf -- '%s' "${src_type}"
+	printf -- '%s' "${src_type}"
+
+	return 0
+	)
 }
 
 __install () {
-    local src_type=$(get_srctype ${1:-})
-    local srcdir="${ROOT}/${src_type}"
+	(
+	src_type=$(get_srctype ${1:-})
 
-    [ ! -d "${srcdir}" ] && \
-        printf -- '`%s` not found.\n' "${srcdir}" && \
-        return 1
+	[ ! -d "${ROOT}/${src_type}" ] && \
+		printf -- '`%s` not found.\n' "${src_type}" && \
+		return 1
 
-    printf -- '-> create symlinks\n'
-    create_symlinks "${srcdir}" "${DESTINATION}"
+	printf -- '-> create symlinks\n'
+	create_symlinks "${ROOT}/${src_type}" "${DESTINATION}"
+	[ -d "${ROOT}/_common" ] && create_symlinks "${ROOT}/_common" "${DESTINATION}"
 
-    printf -- '-> module install\n'
-    [ -f "${srcdir}.sh" ] && \
-        . "${srcdir}.sh" && \
-        module_install
+	printf -- '-> module install\n'
+	[ -f "${ROOT}/${src_type}.sh" ] && \
+		. "${ROOT}/${src_type}.sh" && \
+		module_install
+
+	return 0
+	)
 }
 
 __uninstall () {
-    local src_type=$(get_srctype ${1:-})
-    local srcdir="${ROOT}/${src_type}"
+	(
+	src_type=$(get_srctype ${1:-})
 
-    printf -- '-> module uninstall\n'
-    [ ! -d "${srcdir}" ] && \
-        printf -- '`%s` not found.\n' "${srcdir}" && \
-        return 1
+	[ ! -d "${ROOT}/${src_type}" ] && \
+		printf -- '`%s` not found.\n' "${srcdir}" && \
+		return 1
 
-    [ -f "${srcdir}.sh" ] && \
-        . "${srcdir}.sh" && \
-        module_uninstall
+	printf -- '-> module uninstall\n'
+	[ -f "${ROOT}/${src_type}.sh" ] && \
+		. "${ROOT}/${src_type}.sh" && \
+		module_uninstall
 
-    printf -- '-> delete symlinks\n'
-    remove_dangling_symlinks "${srcdir}" "${DESTINATION}"
-    remove_symlinks "${srcdir}" "${DESTINATION}"
+	printf -- '-> delete symlinks\n'
+	remove_dangling_symlinks "${ROOT}/${src_type}" "${DESTINATION}"
+	remove_symlinks "${ROOT}/${src_type}" "${DESTINATION}"
+	[ -d "${ROOT}/_common" ] && remove_symlinks "${ROOT}/_common" "${DESTINATION}"
+
+	return 0
+	)
 }
 
 __sync () {
-    local src_type=$(get_srctype ${1:-})
-    local srcdir="${ROOT}/${src_type}"
-    [ ! -d "${srcdir}" ] && \
-        printf -- '`%s` not found.\n' "${srcdir}" && \
-        return 1
+	(
+	src_type=$(get_srctype ${1:-})
+	srcdir="${ROOT}/${src_type}"
+	[ ! -d "${srcdir}" ] && \
+		printf -- '`%s` not found.\n' "${srcdir}" && \
+		return 1
 
-    remove_dangling_symlinks "${srcdir}" "${DESTINATION}"
+	remove_dangling_symlinks "${srcdir}" "${DESTINATION}"
+
+	return 0
+	)
 }
 
 case "${1:-}" in
-    'install' | '') __install "${2:-}" ;;
-    'uninstall') __uninstall ;;
-    'sync') __sync ;;
-    'help') die 0;;
-    *) die 1;;
+	'install' | '') __install "${2:-}" ;;
+	'uninstall') __uninstall ;;
+	'sync') __sync ;;
+	'help') die 0;;
+	*) die 1;;
 esac
 
